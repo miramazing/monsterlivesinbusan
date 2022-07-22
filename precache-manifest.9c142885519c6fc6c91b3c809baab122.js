b4d63a98cc98e3b6972bd2c1e837a525ebb844bc
mira <mira.jo.kr@gmail.com>
2022-07-22 21:39:01 +0900

    Updates

---
+++ precache-manifest.9c142885519c6fc6c91b3c809baab122.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85c4bb3fed63c69ad7724917ef8c7c11",
+    "url": "/monsterlivesinbusan/index.html"
+  },
+  {
+    "revision": "550e5574c855e45e6d0d",
+    "url": "/monsterlivesinbusan/static/css/main.182fc3a4.chunk.css"
+  },
+  {
+    "revision": "adf99c7cc1bc5fa4f2cd",
+    "url": "/monsterlivesinbusan/static/js/2.6378ae44.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/monsterlivesinbusan/static/js/2.6378ae44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "550e5574c855e45e6d0d",
+    "url": "/monsterlivesinbusan/static/js/main.1fd9803a.chunk.js"
+  },
+  {
+    "revision": "4281c7b430771d4935d8",
+    "url": "/monsterlivesinbusan/static/js/runtime-main.e3ce90b3.js"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/monsterlivesinbusan/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "6694eb0ed74d5d56b796457dcb73f8c1",
+    "url": "/monsterlivesinbusan/static/media/img1.6694eb0e.jpg"
+  },
+  {
+    "revision": "be3f79f1fdedd5da00e24994f0ff77eb",
+    "url": "/monsterlivesinbusan/static/media/leaflet.be3f79f1.png"
+  },
+  {
+    "revision": "e5cf9278a2daf59a83752c6ca8da7833",
+    "url": "/monsterlivesinbusan/static/media/painting1.e5cf9278.jpg"
+  },
+  {
+    "revision": "c21cd6f4456efa24be6cc8a4e2d55a7d",
+    "url": "/monsterlivesinbusan/static/media/painting2.c21cd6f4.jpg"
+  },
+  {
+    "revision": "1c528ed43ddbe271368f2b7a1c379d4a",
+    "url": "/monsterlivesinbusan/static/media/poster2.1c528ed4.jpg"
+  }
+]);
